8554d12b9240187cba1ebffcddb1437f867cf012
siyaonthedot <mbambosiyabonga@gmail.com>
2019-01-30 23:03:46 +0200

    test

---
+++ GMS.DataService/GymService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="GymService.asmx.cs" Class="GMS.DataService.GymService" %>
---
+++ GMS.DataService/GymService.asmx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GMS.DataService.DataContext;
+using System.Web.Services;
+
+namespace GMS.DataService
+{
+    /// <summary>
+    /// Summary description for GymService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class GymService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public object GetMember(int id)
+        {
+            using (gymEntities db = new gymEntities())
+            {
+                var member = db.Members.Find(id);
+
+                return member;
+            }
+
+                
+        }
+    }
+}
